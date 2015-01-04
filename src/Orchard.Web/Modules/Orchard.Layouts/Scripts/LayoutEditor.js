ea2c3b7ba0920661c6aa071b9eb0dfc0c9f481ef
Daniel Stolt <daniel@ideliverable.com>
2015-01-04 23:59:36 +0100

    Added panel action for editing content element.

--- src/Orchard.Web/Modules/Orchard.Layouts/Scripts/LayoutEditor.js
+++ src/Orchard.Web/Modules/Orchard.Layouts/Scripts/LayoutEditor.js
@@ -151,6 +151,10 @@ angular
             scope: { element: "=" },
             controller: function ($scope, $element) {
                 elementConfigurator.addElementFunctions($scope, $element);
+                $scope.edit = function (e) {
+                    // SIPKE: This is where you invoke the edit dialog for the content element!
+                    console.log("Edit " + $scope.element.contentType);
+                };
             },
             templateUrl: baseUrl.get() + "/Templates/orc-layout-content.html",
             replace: true
--- src/Orchard.Web/Modules/Orchard.Layouts/Scripts/LayoutEditor/Directives/Content.js
+++ src/Orchard.Web/Modules/Orchard.Layouts/Scripts/LayoutEditor/Directives/Content.js
@@ -6,6 +6,10 @@
             scope: { element: "=" },
             controller: function ($scope, $element) {
                 elementConfigurator.addElementFunctions($scope, $element);
+                $scope.edit = function (e) {
+                    // SIPKE: This is where you invoke the edit dialog for the content element!
+                    console.log("Edit " + $scope.element.contentType);
+                };
             },
             templateUrl: baseUrl.get() + "/Templates/orc-layout-content.html",
             replace: true
