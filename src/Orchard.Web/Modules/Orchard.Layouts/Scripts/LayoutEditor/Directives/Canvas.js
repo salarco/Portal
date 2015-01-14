5775559d39507061ccc006fbe820103aa7a5a7f5
Daniel Stolt <daniel@ideliverable.com>
2015-01-14 10:01:43 +0100

    Various improvements to inline editing experience.

--- src/Orchard.Web/Modules/Orchard.Layouts/Scripts/LayoutEditor/Directives/Canvas.js
+++ src/Orchard.Web/Modules/Orchard.Layouts/Scripts/LayoutEditor/Directives/Canvas.js
@@ -27,46 +27,59 @@
                     return host.addElement(contentType);
                 };
 
-                $scope.activateInlineEditing = function () {
-                    $scope.element.inlineEditingIsActive = true;
-                    var firstContentEditorId = $("#layout-canvas-" + $scope.$id + " .layout-content-markup").first().attr("id");
-                    tinymce.init({
-                        selector: "#layout-canvas-" + $scope.$id + " .layout-content-markup",
-                        theme: "modern",
-                        schema: "html5",
-                        plugins: [
-                            "advlist autolink lists link image charmap print preview hr anchor pagebreak",
-                            "searchreplace wordcount visualblocks visualchars code fullscreen",
-                            "insertdatetime media nonbreaking table contextmenu directionality",
-                            "emoticons template paste textcolor colorpicker textpattern",
-                            "fullscreen autoresize"
-                        ],
-                        toolbar: "undo redo cut copy paste | bold italic | bullist numlist outdent indent formatselect | alignleft aligncenter alignright alignjustify ltr rtl | link unlink charmap | code fullscreen close",
-                        setup: function (editor) {
-                            editor.addButton("close", {
-                                text: "Close",
-                                tooltip: "Deactivate inline editing",
-                                icon: false,
-                                onclick: function() {
-                                    tinymce.remove("#layout-canvas-" + $scope.$id + " .layout-content-markup");
-                                    $scope.element.inlineEditingIsActive = false;
-                                }
-                            });
-                        },
-                        convert_urls: false,
-                        valid_elements: "*[*]",
-                        // Shouldn't be needed due to the valid_elements setting, but TinyMCE would strip script.src without it.
-                        extended_valid_elements: "script[type|defer|src|language]",
-                        //menubar: false,
-                        statusbar: false,
-                        skin: "orchardlightgray",
-                        inline: true,
-                        fixed_toolbar_container: "#layout-canvas-" + $scope.$id + " > .layout-toolbar-container",
-                        init_instance_callback: function (editor) {
-                            if (editor.id == firstContentEditorId)
-                                tinymce.execCommand("mceFocus", false, editor.id);
-                        }
-                    });
+                $scope.toggleInlineEditing = function () {
+                    if (!$scope.element.inlineEditingIsActive) {
+                        $scope.element.inlineEditingIsActive = true;
+                        // HACK: Extremely ugly and brittle hack to avoid layout from jumping around when editor loses focus.
+                        $element.find(".layout-toolbar-container").css("min-height", "83px");
+                        var firstContentEditorId = $("#layout-canvas-" + $scope.$id + " .layout-content-markup").first().attr("id");
+                        tinymce.init({
+                            selector: "#layout-canvas-" + $scope.$id + " .layout-content-markup",
+                            theme: "modern",
+                            schema: "html5",
+                            plugins: [
+                                "advlist autolink lists link image charmap print preview hr anchor pagebreak",
+                                "searchreplace wordcount visualblocks visualchars code fullscreen",
+                                "insertdatetime media nonbreaking table contextmenu directionality",
+                                "emoticons template paste textcolor colorpicker textpattern",
+                                "fullscreen autoresize"
+                            ],
+                            toolbar: "undo redo cut copy paste | bold italic | bullist numlist outdent indent formatselect | alignleft aligncenter alignright alignjustify ltr rtl | link unlink charmap | code fullscreen close",
+                            //setup: function (editor) {
+                            //    editor.addButton("close", {
+                            //        text: "Close",
+                            //        tooltip: "Deactivate inline editing",
+                            //        icon: false,
+                            //        onclick: function() {
+                            //            tinymce.remove("#layout-canvas-" + $scope.$id + " .layout-content-markup");
+                            //            // HACK: Extremely ugly and brittle hack to avoid layout from jumping around when editor loses focus.
+                            //            $element.find(".layout-toolbar-container").css("min-height", "");
+                            //            $scope.element.inlineEditingIsActive = false;
+                            //        }
+                            //    });
+                            //},
+                            convert_urls: false,
+                            valid_elements: "*[*]",
+                            // Shouldn't be needed due to the valid_elements setting, but TinyMCE would strip script.src without it.
+                            extended_valid_elements: "script[type|defer|src|language]",
+                            //menubar: false,
+                            statusbar: false,
+                            skin: "orchardlightgray",
+                            inline: true,
+                            fixed_toolbar_container: "#layout-canvas-" + $scope.$id + " > .layout-toolbar-container",
+                            init_instance_callback: function (editor) {
+                                if (editor.id == firstContentEditorId)
+                                    tinymce.execCommand("mceFocus", false, editor.id);
+
+                            }
+                        });
+                    }
+                    else {
+                        tinymce.remove("#layout-canvas-" + $scope.$id + " .layout-content-markup");
+                        // HACK: Extremely ugly and brittle hack to avoid layout from jumping around when editor loses focus.
+                        $element.find(".layout-toolbar-container").css("min-height", "");
+                        $scope.element.inlineEditingIsActive = false;
+                    }
                 };
 
                 $(document).on("cut copy paste", function (e) {
@@ -96,8 +109,17 @@
                 element.find(".layout-toolbar-container").click(function (e) {
                     e.stopPropagation();
                 });
+                // Intercept mousedown on canvas while in inline editing mode to 
+                // prevent current editor from losing focus.
+                element.mousedown(function (e) {
+                    if (scope.element.canvas.inlineEditingIsActive) {
+                        e.preventDefault();
+                        e.stopPropagation();
+                    }
+                })
                 // Unfocus and unselect everything on click outside of canvas.
                 $(window).click(function (e) {
+                    // Except when in inline editing mode.
                     if (!scope.element.canvas.inlineEditingIsActive) {
                         scope.$apply(function () {
                             scope.element.canvas.activeElement = null;
